b945600b728f488e1c3ac7409f33a9ad6133585c
mmerelli <michelemerelli.8@gmail.com>
2025-05-22 19:53:30 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
    CONFIG – Secrets injected by GitHub Actions
 --------------------------------- */
 const CONFIG = {
-  clientId: "YOUR_CLIENT_ID", // Will be replaced by GitHub Actions
-  clientSecret: "YOUR_CLIENT_SECRET", // Will be replaced by GitHub Actions
-  refreshToken: "YOUR_REFRESH_TOKEN" // Will be replaced by GitHub Actions
+  clientId: "STRAVA_CLIENT_ID", // Will be replaced by GitHub Actions
+  clientSecret: "STRAVA_CLIENT_SECRET", // Will be replaced by GitHub Actions
+  refreshToken: "STRAVA_REFRESH_TOKEN" // Will be replaced by GitHub Actions
 };
