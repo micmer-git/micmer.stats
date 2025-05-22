edac189003a178df0a90ba65885d8f0c4df279cc
micmer <56996310+micmer-git@users.noreply.github.com>
2025-05-22 10:27:11 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+/* ---------------------------------
+   CONFIG – Secrets injected by GitHub Actions
+--------------------------------- */
+const CONFIG = {
+  clientId: "YOUR_CLIENT_ID", // Will be replaced by GitHub Actions
+  clientSecret: "YOUR_CLIENT_SECRET", // Will be replaced by GitHub Actions
+  refreshToken: "YOUR_REFRESH_TOKEN" // Will be replaced by GitHub Actions
+};
