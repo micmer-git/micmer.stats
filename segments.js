7670c87cc7bf22490ee5ad39b33a66e025eb905d
micmer <56996310+micmer-git@users.noreply.github.com>
2025-05-24 18:02:10 +0200

    Create segments.js

---
+++ segments.js
@@ -0,0 +1,6 @@
+// Example – replace IDs with your Strava climb segment IDs:
+export default {
+  1234567: { name:'Passo dello Stelvio', avg_grad:7.5, length_km:13 },
+  7654321: { name:'Monte Grappa (Seren)', avg_grad:8.1, length_km:18.2 },
+  /* … */
+};
